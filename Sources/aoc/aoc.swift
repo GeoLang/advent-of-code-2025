608b8837aed990b6832bb2880ccca53b6695c10e
George Ramzi <George.Langroudi@outlook.com>
2025-12-05 10:47:39 +0000

    Add Day 5 solution

--- Sources/aoc/aoc.swift
+++ Sources/aoc/aoc.swift
@@ -7,21 +7,6 @@
 import ArgumentParser
 
 
-enum Day: Int, ExpressibleByArgument {
-    case day1 = 1
-    case day2 = 2
-    case day3 = 3
-    case day4 = 4
-    case day5 = 5
-    case day6 = 6
-    case day7 = 7
-    case day8 = 8
-    case day9 = 9
-    case day10 = 10
-    case day11 = 11
-    case day12 = 12
-}
-
 @main
 struct aoc: ParsableCommand {
     static let configuration: CommandConfiguration = CommandConfiguration(
@@ -29,6 +14,6 @@ struct aoc: ParsableCommand {
         abstract: "Run Advent of Code challenges.",
         usage: "aoc --day <day_number>",
         discussion: "Select the day to run using the --day argument.",
-        subcommands: [Day1.self, Day2.self, Day3.self, Day4.self]
+        subcommands: [Day1.self, Day2.self, Day3.self, Day4.self, Day5.self]
     )
 }
---
+++ Sources/aoc/day5.swift
@@ -0,0 +1,114 @@
+import ArgumentParser
+import Foundation
+
+
+struct Day5: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "day5",
+        abstract: "Run the Day 5 Advent of Code challenge."
+    )
+
+    @Argument(help: "Path to the input file for Day 5.")
+    var inputFilePath: String
+
+    @Option(name: .shortAndLong, help: "The task to run, 1 or 2.")
+    var task: Int
+
+    func run() throws {
+        /// Save the start time to measure how long the solution took. 
+        let start = DispatchTime.now()
+
+        let fileContents = try String(contentsOfFile: inputFilePath)
+        let rawLists = fileContents.split(separator: "\n")
+        var ranges: [String] = []
+        var ingredients: [Int] = []
+
+        rawLists.forEach { element in
+            if element.contains("-") {
+                ranges.append(String(element))
+            }
+            else {
+                if element != "" {
+                    ingredients.append(Int(element)!)
+                }
+            }
+        }
+
+        if task == 1 {
+            task1(ranges: ranges, ingredients: ingredients)
+        } else if task == 2 {
+            task2(ranges: ranges)
+        } else {
+            print("Invalid task number: '\(task)'. Should be 1 or 2.")
+        }
+
+        /// Check the end time to measure how long we took.
+        let end = DispatchTime.now()
+        let nanos = end.uptimeNanoseconds - start.uptimeNanoseconds
+        let seconds = Double(nanos) / 1_000_000_000
+        print("Finished in: \(seconds)s")
+    }
+
+    fileprivate func task1(ranges: [String], ingredients: [Int]) {
+        let freshIngredients = ingredients.map { isFresh(ranges: ranges, ingredient: $0) }
+        print("Fresh ingredients: \(freshIngredients.count(where: {$0}))")
+    }
+
+    fileprivate func task2(ranges rawRanges: [String]) {
+        let ranges = rawRanges.map { stringToClosedRange($0) }
+
+        let out = simplifyRanges(ranges: ranges).reduce(0) { $0 + $1.count }
+        print("Total fresh: \(out)")
+    }
+
+    /// Find overlapping ranges, and simplify them.
+    /// - Parameter ranges: A list of `ClosedRange<Int>` to simplify.
+    /// - Returns: A list of `ClosedRange<Int>` with overlapping ranges combined.
+    fileprivate func simplifyRanges(ranges: [ClosedRange<Int>]) -> [ClosedRange<Int>] {
+        let reducedRanges = ranges.sorted { $0.lowerBound < $1.lowerBound }
+        var mergedRanges: [ClosedRange<Int>] = [reducedRanges[0]]
+
+        var index = 1
+        while index < reducedRanges.count {
+            let last = mergedRanges[mergedRanges.count - 1]
+            let next = reducedRanges[index]
+
+            if last.overlaps(next) {
+                mergedRanges[mergedRanges.count - 1] =
+                    last.lowerBound ... max(last.upperBound, next.upperBound)
+            } else {
+                mergedRanges.append(next)
+            }
+
+            index += 1
+        }
+
+        return mergedRanges
+    }
+
+    /// Convert a `String`` for a range (i.e. "xxx-yyyy") into a `ClosedRange<Int>`
+    /// - Parameter range: The string formatted as "xxx-yyyy"
+    /// - Returns: A `ClosedRange<Int>` with lowerBound and upperBound set based on the range.
+    fileprivate func stringToClosedRange(_ range: String) -> ClosedRange<Int> {
+        let splitRange = range.split(separator: "-")
+        let lhs = Int(splitRange[0])!
+        let rhs = Int(splitRange[1])!
+        return lhs...rhs
+    }
+
+    /// Check whether a specific ingredient falls wtihin any range in `ranges`
+    /// - Parameters:
+    ///   - ranges: A list of `[ClosedRange<Int>]` to check the ingredient falls within.
+    ///   - ingredient: The specific ingredient being checked
+    ///
+    /// - Returns: `true` if the ingredient is in any range, otherwise `false`.
+    fileprivate func isFresh(ranges rawRanges: [String], ingredient: Int) -> Bool { 
+        let ranges = rawRanges.map(stringToClosedRange)
+        for range in ranges {
+            if range.contains(ingredient) {
+                return true
+            }
+        }
+        return false
+    }
+}
